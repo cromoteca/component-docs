24cce355852c0820596c829cf2bc240692c76ecc
Luciano Vernaschi <luciano@vaadin.com>
2023-03-15 18:09:28 +0100

    Use Notification instead of console.log

--- frontend/views/checkbox/CheckboxGroupSimple.tsx
+++ frontend/views/checkbox/CheckboxGroupSimple.tsx
@@ -1,12 +1,15 @@
 import { Checkbox } from "@hilla/react-components/Checkbox.js";
 import { CheckboxGroup } from "@hilla/react-components/CheckboxGroup.js";
+import { Notification } from "@hilla/react-components/Notification.js";
 
 export default function CheckboxGroupSimple() {
   return (
     <CheckboxGroup
       label="Export data"
       theme="vertical"
-      onValueChanged={({ detail: { value } }) => console.log(value)}
+      onValueChanged={({ detail: { value } }) =>
+        Notification.show(`Current selection: ${value}`)
+      }
     >
       <Checkbox value="0" label="Order ID" />
       <Checkbox value="1" label="Product name" />
