b8b74d477b7662800df7586841ed413557c4a3eb
Luciano Vernaschi <luciano@vaadin.com>
2023-03-29 18:10:50 +0200

    Replace `hasControls` with `stepButtonsVisible`

--- frontend/views/number-field/NumberFieldStepper.tsx
+++ frontend/views/number-field/NumberFieldStepper.tsx
@@ -4,16 +4,22 @@ import { NumberField } from "@hilla/react-components/NumberField.js";
 export default function NumberFieldStepper() {
   return (
     <HorizontalLayout theme="spacing">
-      <NumberField label="Adults" hasControls value="1" min={1} />
+      <NumberField label="Adults" stepButtonsVisible value="1" min={1} />
       <NumberField
         label="Children"
-        hasControls
+        stepButtonsVisible
         value="0"
         min={0}
         max={9}
         helperText="Age 2-12"
       />
-      <NumberField label="Infants" hasControls value="0" min={0} max={9} />
+      <NumberField
+        label="Infants"
+        stepButtonsVisible
+        value="0"
+        min={0}
+        max={9}
+      />
     </HorizontalLayout>
   );
 }
